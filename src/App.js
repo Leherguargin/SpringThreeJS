51a01365e11a29714f4c80c11d1d370c250f46c8
Leherguargin <43795159+Leherguargin@users.noreply.github.com>
2021-12-18 15:01:19 +0100

    all spring rendered

--- src/App.js
+++ src/App.js
@@ -69,8 +69,8 @@ class App extends React.Component {
         points[i].push([
           [x1, y1, z1],
           [x2, y2, z2],
-          [x3, y3, z3],
-          [x4, y4, z4]
+          [x4, y4, z4],
+          [x3, y3, z3]
         ]);
       }
     }
